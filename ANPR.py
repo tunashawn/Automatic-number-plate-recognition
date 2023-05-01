6e59935c69eccc922ae34d8bb6dd89cfc4ac720d
Tuna Shawn <73065657+tunashawn@users.noreply.github.com>
2023-05-02 03:10:28 +0700

    Update ANPR.py
    added comments

--- ANPR.py
+++ ANPR.py
@@ -4,27 +4,10 @@ import cv2
 import sys
 
 
-def paddleocr_predict(img):
-    img = cv2.resize(img, (0, 0), fx=3, fy=3)
-
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    ocr = PaddleOCR(use_angle_cls=True, lang='en', det=False)
-    result = ocr.ocr(gray_image)
-
-    plate_number = ""
-
-    for idx in range(len(result)):
-        res = result[idx]
-        for line in res:
-            plate_number += (line[-1][0] + " ")
-    
-    return plate_number
-
-
+# Get all license plates in image
 def get_plates(result, img):
-    images = []
-    boxes = result[0].boxes
+    images = [] # Store all license plates
+    boxes = result[0].boxes # List of all coordinates of license plates
     img = img.copy()
     for b in boxes:
         x1 = int(b.xyxy[0][0])
@@ -34,10 +17,10 @@ def get_plates(result, img):
         images.append(img[y1:y2, x1:x2].copy())
     return images
 
-
+# OCR
 def get_LP_number(result, img):
     plates = get_plates(result, img)
-    plate_numbers = []
+    plate_numbers = [] # Store all LP number 
     
     for plate in plates:
         number = paddleocr_predict(plate)
@@ -45,47 +28,56 @@ def get_LP_number(result, img):
     
     return plate_numbers
 
-
+# Process single image
+# Draw rectangle around plates and LP number
 def draw_box(result, img):
-
-    boxes = result[0].boxes
-    plate_numbers = get_LP_number(result, img)
-
-    for b, pnum in zip(boxes, plate_numbers):
+    boxes = result[0].boxes # All coordinates of plates
+    plate_numbers = get_LP_number(result, img) # All predicted LP number
+    
+    # For each LP coordinates and each predicted LP number of that LP
+    for b, pnum in zip(boxes, plate_numbers): 
         x1 = int(b.xyxy[0][0])
-        y1 = int(b.xyxy[0][1]) - 20
+        y1 = int(b.xyxy[0][1]) - 20 # Small adjust to make it looks better
         x2 = int(b.xyxy[0][2])
         y2 = int(b.xyxy[0][3])
+        # Draw rectangle around the LP
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # Fill background of the predicted LP number
         cv2.rectangle(img, (x1, y1 + 22), ((x2), (y1)), (0, 255, 0), -1)
-        text_xy = (x1 + 2, y1 +18)
+        text_xy = (x1 + 2, y1 +18)  # Coordinate of predicted LP number
+        # add predicted LP number
         # img, text, position, font, font_scale, color, thickness
         cv2.putText(img, pnum, text_xy, 0, 0.7, 0, 2) 
     
     return img
-    
+
+# Process video 
 def video_draw_box(vid_path, model):
     cap = cv2.VideoCapture(vid_path)
+    
+    # width and height must be correct to save output
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+    
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter('output.avi', fourcc, 20, (width, height))
     
+    # Process each frame of the video
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+       
+        result = model(frame)   # Predict position of LPs
+        frame = draw_box(result, frame) # Draw rectangle and predicted LP number for current frame
         
-        result = model(frame)
-        frame = draw_box(result, frame)
-        
-        # write the flipped frame
-        out.write(frame)
+        out.write(frame)    # Write to output.avi
         cv2.imshow('frame', frame)
+        
         if cv2.waitKey(1) == ord('q'):
             break
-    # Release everything if job is finished
+    
     # cap.release()
     out.release()
     cv2.destroyAllWindows()
